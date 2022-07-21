fbe44a23f9b057e71302a47a666e843e80802ec3
DryDish <57399961+DryDish@users.noreply.github.com>
2022-07-22 01:23:19 +0200

    update build script docker compose call

--- hook-server/src/utils/scripts/build_script.sh
+++ hook-server/src/utils/scripts/build_script.sh
@@ -16,23 +16,23 @@ fi
 
 echo "Taking down any previous instance of the test environment..."
 # Remove any lingering environments
-docker-compose down
+docker compose down
 
 echo "Running the test environment..."
 # Run the tests
-docker-compose up $TEST_IMAGE --build --exit-code-from $TEST_IMAGE 
+docker compose up $TEST_IMAGE --build --exit-code-from $TEST_IMAGE 
 test_result=$?
 
 echo "Cleaning up..."
 # Remove environments
-docker-compose down
+docker compose down
 
 # If express-app-tests service does not return errors
 if [ $test_result -eq 0 ]; then
 
     echo "Tests passed! Building the project..."
     # Build the project
-    docker-compose build express-app-prod addresses-db-prod
+    docker compose build express-app-prod addresses-db-prod
 
     echo "Logging in to DockerHub..."
     # Log in to DockerHub using ENV for username and token
@@ -43,7 +43,7 @@ if [ $test_result -eq 0 ]; then
  
         echo "Login Success! Updating images..."
         # Push the images to DockerHub
-        docker-compose push express-app-prod addresses-db-prod 
+        docker compose push express-app-prod addresses-db-prod 
         upload_result=$?
 
         if [ $upload_result -eq 0 ]; then
