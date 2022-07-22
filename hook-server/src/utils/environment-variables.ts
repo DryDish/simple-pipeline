af5bc236256faee0ae303e1b47581d67397b23ea
DryDish <57399961+DryDish@users.noreply.github.com>
2022-07-22 04:56:43 +0200

    update environment variable population script

--- hook-server/src/utils/environment-variables.ts
+++ hook-server/src/utils/environment-variables.ts
@@ -1,18 +1,15 @@
 import { error } from "./logger.js";
 import { exit } from "shelljs";
 
-const SYSTEM_USER: string = process.env.USER!;
-
 const folderNameLength: number = "hook-server".length;
 const hookIndex: number = __dirname.lastIndexOf("hook-server") + folderNameLength;
 
 const SERVER_ROOT: string = __dirname.slice(0, hookIndex);
 
-if (!SERVER_ROOT.includes("hook-server") || SYSTEM_USER == undefined) {
+if (!SERVER_ROOT.includes("hook-server")) {
 	error("Critical ENVs not set, aborting!");
-	error(`SYSTEM_USER was set to: '${SYSTEM_USER}'`);
 	error(`SERVER_ROOT was set to: '${SERVER_ROOT}'`);
 	exit(1);
 }
 
-export { SERVER_ROOT, SYSTEM_USER };
+export { SERVER_ROOT };
