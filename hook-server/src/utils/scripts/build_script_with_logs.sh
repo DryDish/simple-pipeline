5b112ed726b871562fbf1ba3065b3f6fb4fa1ec1
DryDish <57399961+DryDish@users.noreply.github.com>
2022-07-22 04:58:47 +0200

    move log storage location

--- hook-server/src/utils/scripts/build_script_with_logs.sh
+++ hook-server/src/utils/scripts/build_script_with_logs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Runs the build script, adds timestamps and appends the output to a log folder with a filename of the date at runtime
-./scripts/build_script.sh 2>&1 | awk '{ print strftime("[%Y-%m-%d %H:%M:%S] -- "), $0; fflush(); }' | tee -a /var/log/hook-server/$(date +"%F").txt
+./scripts/build_script.sh 2>&1 | awk '{ print strftime("[%Y-%m-%d %H:%M:%S] -- "), $0; fflush(); }' | tee -a /home/ubuntu/.log/hook-server/$(date +"%F").txt
 
 # Capture exit code of build_script.sh
 EXIT_CODE=${PIPESTATUS[0]}
