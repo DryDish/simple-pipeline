6c0409f51d7866cb31ded6adda3f192eed4ec2bf
DryDish <57399961+DryDish@users.noreply.github.com>
2022-07-22 16:48:07 +0200

    fix bug of incorrect user added to docker group

--- linux-files/install-scripts/install-docker.sh
+++ linux-files/install-scripts/install-docker.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/bash
 # Must be run as sudo
 
 # Add required dependecies
@@ -12,7 +12,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
 
 # Add docker repository
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | \
-    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Update package manager and install docker and docker compose
 apt update
@@ -21,7 +21,7 @@ apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 # add docker to usergroup
 groupadd docker
-usermod -aG docker $USER
+usermod -aG docker $(logname)
 
 echo ""
 echo "Install completed, please reboot before you attempt to use docker."
