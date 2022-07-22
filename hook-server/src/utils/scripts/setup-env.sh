c0ce571d29a5594ace14e767b56a32354be67587
DryDish <57399961+DryDish@users.noreply.github.com>
2022-07-22 04:57:36 +0200

    update setup shell script

--- hook-server/src/utils/scripts/setup-env.sh
+++ hook-server/src/utils/scripts/setup-env.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 # Export any and all required ENVs, to be executed on the build server
-export PLACEHOLDER=PLACEHOLDER;
-
 export TEST_IMAGE='express-app-tests'
-export DOCKER_USER='placeholder-user'
-export DOCKER_TOKEN='placeholder-token'
+export APP_PORT=3000
+export MYSQL_ROOT_PASSWORD='12345678'
+export MYSQL_HOST='addresses-db-prod'
+export SERVICE_HOST='express-app-prod'
+export MYSQL_USER='test'
+export MYSQL_PASSWORD='12345678'
+export DOCKER_USER='username'
+export DOCKER_TOKEN='password
